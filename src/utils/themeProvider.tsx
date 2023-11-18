1fdf0572eda5eb239ee04596103a2e084fcb356e
riejan029 <eguitariejan@gmail.com>
2023-11-18 20:34:38 +0800

    fix display

--- src/utils/themeProvider.tsx
+++ src/utils/themeProvider.tsx
@@ -1,27 +1,32 @@
 import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#414145",
-      contrastText: "#FFF",
-    },
-    secondary: {
-      main: "#FFF",
-      contrastText: "#000",
-    },
-  },
-  typography: {
-    fontFamily: "Agbalumo, sans-serif",
-    htmlFontSize: 14,
-  },
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: {
-        body: {
-          margin: 0,
+    palette: {
+        primary: {
+            main: "#414145",
+            contrastText: "#FFF",
         },
-      },
+        secondary: {
+            main: "#FFF",
+            contrastText: "#000",
+        },
     },
-  },
+    typography: {
+        fontFamily: "Agbalumo, sans-serif",
+        htmlFontSize: 14,
+    },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: {
+                body: {
+                    margin: 0,
+                },
+                html: {
+                    "@media (max-width:1366px)": {
+                        fontSize: "12px",
+                    },
+                },
+            },
+        },
+    },
 });
